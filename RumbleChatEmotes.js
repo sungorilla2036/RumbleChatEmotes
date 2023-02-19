d2f0b961e77c84f816713afd2db0d83a59434333
Darg <darg@cpusa2036.com>
2023-02-18 20:07:39 -0700

    Bump version number

--- RumbleChatEmotes.js
+++ RumbleChatEmotes.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rumble Chat Emojis
 // @namespace    http://tampermonkey.net/
-// @version      0.45
+// @version      0.46
 // @description  Replace keywords with emojis
 // @author       Infrared Labs
 // @match        https://rumble.com/*
