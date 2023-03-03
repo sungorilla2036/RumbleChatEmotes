afc3ffa1d24b560c44b2e07e4e32653b3a775873
AmirioTheGreat <119095193+AmirioTheGreat@users.noreply.github.com>
2023-03-03 15:14:16 -0800

    Update RumbleChatEmotes.js

--- RumbleChatEmotes.js
+++ RumbleChatEmotes.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rumble Chat Emojis
 // @namespace    http://tampermonkey.net/
-// @version      0.54
+// @version      0.55
 // @description  Replace keywords with emojis
 // @author       Infrared Labs
 // @match        https://rumble.com/*
@@ -23,7 +23,7 @@
     }
 
     .RumbleChatEmotes-emoteMenu div {
-      height: 440px;
+      height: 450px;
       overflow: scroll;
       overscroll-behavior: contain;
     }
